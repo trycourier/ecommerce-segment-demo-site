64877be7856d822834023df9b7613cbdd992c7fb
send_on <ohan88@gmail.com>
2021-11-03 11:36:00 -0700

    add name and email to profile call

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -1,9 +1,11 @@
 import fetch from "isomorphic-unfetch";
 import { config } from "../../utils/config";
 
+const includeList = "name,email,last_brand_viewed,last_order_date,last_order_number,last_product_added,last_product_image,last_product_viewed,most_frequent_product_category_purchased,number_of_orders_completed_all_time,product_view_counts,unique_list_products_purchased,upt_number_of_products_purchased"
+
 export const getUser = async (anonymous_id, type = "traits") => {
   const response = await fetch(
-    `https://profiles.segment.com/v1/spaces/${config.api.PERSONAS_SPACE_ID}/collections/users/profiles/anonymous_id:${anonymous_id}/${type}?limit=200&class=computed_trait`,
+    `https://profiles.segment.com/v1/spaces/${config.api.PERSONAS_SPACE_ID}/collections/users/profiles/anonymous_id:${anonymous_id}/${type}?include=${includeList}&limit=50`,
     {
       method: "GET",
       headers: {
